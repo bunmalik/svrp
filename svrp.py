ef71fc07da50764c64e68b9c510954a5e7d0808b
bunmalik <bunmalik11@gmail.com>
2021-04-17 12:09:53 +0000

    Update svrp.py

--- svrp.py
+++ svrp.py
@@ -20,6 +20,7 @@ import numpy
 
 model = AbstractModel()
 
+
 #
 # Parameters
 #
@@ -46,7 +47,7 @@ model.ArcDemand = Param(model.I, model.J) #arc demand
 #
 model.X_WS = Param(model.S)
 model.X_EV = Param(model.S)
-model.X_RP = Param(model.S)
+#model.X_RP = Param(initialize=lambda m: model.S[value(model.S)])
 model.X = Var(model.S, bounds=(0.0, model.B))
 model.Y = Var(model.I, model.J, bounds=(0.0, model.B))
 model.Z = Var(model.I, model.J, bounds=(0.0, None))
@@ -61,29 +62,29 @@ model.SecondStageProfit = Var()
 
 def vehicle_num_cap_rule(model):
     return sum(model.X[s] for s in model.S) <= model.B
-model.VehicleNumCapRule = Constraint(expr=vehicle_num_cap_rule)
+model.VehicleNumCapRule = Constraint(rule=vehicle_num_cap_rule)
 
 
 def vehicle_assigned_cap_rule(model,s):
     return sum(model.Y[s,j] for j in model.J if model.Arc[s,j]>=1) == model.X[s]
-model.RequiredDemandRule = Constraint(model.S, expr=vehicle_assigned_cap_rule)
+model.RequiredDemandRule = Constraint(model.S, rule=vehicle_assigned_cap_rule)
 
 def flow_balance_rule(model,d):
     return (sum(model.Y[i,d] for i in model.I if model.Arc[i,d]>=1) - sum(model.Y[d,i] for i in model.I if model.Arc[d,i]>=1)) == 0.0
-model.FlowBalanceRule = Constraint(model.D, expr=flow_balance_rule)
+model.FlowBalanceRule = Constraint(model.D, rule=flow_balance_rule)
 
 def overage_rule(model,i,j):
     return model.Y[i,j] - model.ArcDemand[i,j] <= model.Z[i,j]
-model.OverageRule = Constraint(model.I, model.J, expr=overage_rule)
+model.OverageRule = Constraint(model.I, model.J, rule=overage_rule)
 
 def y_rule(model,i,j):
     return (0.0, model.Y[i,j], model.Arc[i,j]*51)
-model.YRule = Constraint(model.I, model.J, expr=y_rule)
+model.YRule = Constraint(model.I, model.J, rule=y_rule)
 
 #NOTE: (Part H) We have added a constraint to fix X at RP
-def x_fix_rule(model,s):
-    return model.X[s] == model.X_RP[s]
-model.XFixRule = Constraint(model.S, expr=x_fix_rule)
+##def x_fix_rule(model,s):
+##    return model.X[s] == model.X_RP[s]
+##model.XFixRule = Constraint(model.S, rule=x_fix_rule)
 
 #
 # Stage-specific cost computations
@@ -91,12 +92,12 @@ model.XFixRule = Constraint(model.S, expr=x_fix_rule)
 
 def first_stage_profit_rule(model):
     return model.FirstStageProfit == 0.0
-model.ComputeFirstStageProfit = Constraint(expr=first_stage_profit_rule)
+model.ComputeFirstStageProfit = Constraint(rule=first_stage_profit_rule)
 
 def second_stage_profit_rule(model):
     return model.SecondStageProfit - sum(sum(model.Rev[i,j] * model.Y[i,j] - (model.Rev[i,j] + model.Cost[i,j])* model.Z[i,j] \
                                            for i in model.I) for j in model.J) == 0.0
-model.ComputeSecondStageProfit = Constraint(expr=second_stage_profit_rule)
+model.ComputeSecondStageProfit = Constraint(rule=second_stage_profit_rule)
 
 
 
@@ -107,27 +108,36 @@ model.ComputeSecondStageProfit = Constraint(expr=second_stage_profit_rule)
 def total_profit_rule(model):
     return (model.FirstStageProfit + model.SecondStageProfit)
 
-model.Total_Profit_Objective = Objective(expr=total_profit_rule, sense=maximize)
+model.Total_Profit_Objective = Objective(rule=total_profit_rule, sense=maximize)
 
 
 
 # Solve WS for given number of sample realizations with fixed X at X_WS
-numSamples=50
+numSamples=100
 numX=5
 optVal=numpy.array([0 for i in range(numSamples)])
 
 
 
-
+import sys
 for i in range(numSamples):
     datafile='svrp_data.dat'
     #datafile = '../scenariodata/Scenario' + str(i+1) + '.dat'
-    instance = model.create_instance(datafile) 
-    opt = SolverFactory('glpk')
+    instance = model.create_instance(datafile)
+    solvername='glpk'
+    solverpath_folder='C:\\Users\\bunma\\Downloads\\glpk-4.65\\w64' #does not need to be directly on c drive
+    solverpath_exe='C:\\Users\\bunma\\Downloads\\glpk-4.65\\w64\\glpsol' #does not need to be directly on c drive
+    sys.path.append(solverpath_folder)
+    opt=SolverFactory(solvername,executable=solverpath_exe)
     results = opt.solve(instance, tee=True)
-    optVal[i] = results.solution.objective.f.value
+    instance.solutions.store_to(results)
+    #results.write()
+    #instance.pprint()
+    #instance.display()
+    optVal[i] = value(instance.Total_Profit_Objective)
 
-'''
+
+
 # Calculate point est / interval est of objective value
 z_val = 1.96
 ERP = optVal[:].mean()
@@ -138,4 +148,4 @@ ERP_CI_hi = ERP + ERP_halfwidth
 
 print(ERP)
 print (ERP_CI_lo, ERP_CI_hi)
-'''
+
